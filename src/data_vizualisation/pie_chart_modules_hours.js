5df27a8b43dffead1c3c5d03c3fa0bb50c6bbe33
Mike Simard <37785613+mikesimard93@users.noreply.github.com>
2021-01-25 13:56:23 -0500

    Added spin to dashboard

--- src/data_vizualisation/pie_chart_modules_hours.js
+++ src/data_vizualisation/pie_chart_modules_hours.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import PieChart, {
     Series,
@@ -7,36 +8,50 @@ import PieChart, {
     Size,
     Export
 } from 'devextreme-react/pie-chart';
+import Box from "@material-ui/core/Box";
+import Tree from "react-tree-graph";
 
 
 function PieChartHoursModules(props) {
 
     const [data, updateData] = React.useState([]);
+    const [loading, changeLoadStatus] = React.useState(true);
 
     // The useEffect will be called at the beginning AND each time the props.Jobs changes
     // The component will be rendered each time useEffect is executed.
     React.useEffect(() => {
         updateData(props.data)
+        changeLoadStatus(props.loading)
     }, [props.data])
 
         return (
-            <PieChart
-                id="pie"
-                dataSource={data}
-                palette="Bright"
-            >
-                <Series
-                    argumentField="name"
-                    valueField="area"
-                >
-                    <Label visible={true}>
-                        <Connector visible={true} width={1} />
-                    </Label>
-                </Series>
+            <div >
+                {(function () {
+                    if (loading) {
+                        return <Box > <CircularProgress size={40} />
+                            <Box pt={2}>May take several minutes to load..</Box>
+                        </Box>
+                    } else {
+                        return <PieChart
+                            id="pie"
+                            dataSource={data}
+                            palette="Bright"
+                        >
+                            <Series
+                                argumentField="name"
+                                valueField="area"
+                            >
+                                <Label visible={true}>
+                                    <Connector visible={true} width={1} />
+                                </Label>
+                            </Series>
 
 
-                <Export enabled={false} />
-            </PieChart>
+                            <Export enabled={false} />
+                        </PieChart>;
+                    }
+                })()}
+            </div>
         );
 }
 
